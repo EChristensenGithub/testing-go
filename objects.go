8a0db8dc17ba0d10011370dab04973928dd7a3d0
EChristensenGithub <emilcc89@gmail.com>
2022-10-18 10:23:38 +0200

    more files

---
+++ objects.go
@@ -0,0 +1,10 @@
+package main
+
+type Employee struct {
+	Name        string
+	Age         int
+	Designation string
+	Salary      int
+}
+
+var newEmployee = Employee{"Mayank", 30, "Developer", 40}
--- script.go
+++ script.go
@@ -3,11 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	idk()
-	main2()
-}
+	statePopulations := map[string]int{
+		"California": 39250017,
+		"Texas":      27862596,
+		"Florida":    20612439,
+	}
+	for _, v := range statePopulations {
+		fmt.Println(v)
+	}
 
-func idk() {
-	fmt.Println("ihi")
+	a := [4]int{1, 2, 3, 4} // "a" has type [4]int (array of 4 ints)
+	x := a[0:]
+
+	fmt.Println(x)
+
+	buttons := [][]string{
+		{"Some", "Test", "Buttons"},
+		{"Another", "Row"},
+	}
+
+	fmt.Printf("The whole array: %v\n", buttons)
+	fmt.Printf("First member of the array: %v\n", buttons[0])
+	fmt.Printf("Second member of the array: %v\n", buttons[1])
+
+	h := []int{1, 2, 3}
+	fmt.Println(h)
+
+	var s []int
+	s = append(s, 0)
+	fmt.Println(s)
 }
